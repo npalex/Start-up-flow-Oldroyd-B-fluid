9657cd89419e9d21daa41e1ce349e173c17cd860
Nathan Alexander <nalexander460@gmail.com>
2024-05-16 12:58:03 -0700

    Add files via upload

---
+++ setprob.f90
@@ -0,0 +1,34 @@
+subroutine setprob
+
+    implicit none
+    character*25 :: fname
+    integer :: iunit
+	real(kind=8) :: cc,zz,beta,E,Ma
+	common /cparam/ cc,zz,beta,E,Ma 
+ 
+    ! Set the material parameters
+    ! Passed to the Riemann solver rp1.f in a common block
+    iunit = 7
+    fname = 'setprob.data'
+	
+    ! open the unit with new routine from Clawpack 4.4 to skip over
+    ! comment lines starting with #:
+    call opendatafile(iunit, fname)
+	
+	! Elasticity number
+	read(7,*) E
+
+	! viscoelastic mach number:
+	read(7,*) Ma
+
+    ! shear wave propagation speed:
+	cc = 1./Ma
+
+    ! square root of the elasticity number:
+	zz = -dsqrt(E)
+	
+    ! viscosity ratio: (e.g., for a polymer solution,... 
+	! ...beta is the viscosity of the mixture over that of the solvent)
+    read(7,*) beta
+
+end subroutine setprob
