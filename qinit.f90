7cf90b9755db1985368fe01263eeb535c8cb1d71
npalex <nalexander460@gmail.com>
2024-05-18 20:47:32 -0700

    update file

--- qinit.f90
+++ qinit.f90
@@ -1,9 +1,9 @@
 subroutine qinit(meqn,mbc,mx,xlower,dx,q,maux,aux)
 
-    ! Set initial conditions for the q array.
-    ! This default version prints an error message since it should
-    ! not be used directly.  Copy this to an application directory and
-    ! loop over all grid cells to set values of q(1:meqn, 1:mx).
+    c Set initial conditions for the q array.
+    c This default version prints an error message since it should
+    c not be used directly.  Copy this to an application directory and
+    c loop over all grid cells to set values of q(1:meqn, 1:mx).
 
     implicit none
     
@@ -12,7 +12,7 @@ subroutine qinit(meqn,mbc,mx,xlower,dx,q,maux,aux)
     real(kind=8), intent(in) :: aux(maux,1-mbc:mx+mbc)
     real(kind=8), intent(inout) :: q(meqn,1-mbc:mx+mbc)
     
-	! stress-free fluid at rest
+	c stress-free fluid at rest
     q = 0.d0
 
 end subroutine qinit
